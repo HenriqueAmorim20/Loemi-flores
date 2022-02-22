7826685ead34234e5e5fc3f4d31259b4cf86e9dd
Henrique Melo <hacmelo@gmail.com>
2022-02-22 00:35:09 -0300

    Global styles and fonts import

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app class="background">
     <Navbar />
     <v-main>
       <Nuxt />
@@ -20,3 +20,36 @@ export default {
   }
 };
 </script>
+
+<style>
+/* Global styles */
+@font-face {
+  font-family: "cormorantLight";
+  src: url("~/assets/CormorantGaramond-Light.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "cormorant";
+  src: url("~/assets/CormorantGaramond-Regular.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "cormorantBold";
+  src: url("~/assets/CormorantGaramond-Bold.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "assinatura";
+  src: url("~/assets/assinatura.ttf") format("truetype");
+}
+
+* {
+  font-family: "cormorant";
+  color: #706063;
+}
+
+.background {
+  background-color: #dbc9cc !important;
+}
+
+</style>
