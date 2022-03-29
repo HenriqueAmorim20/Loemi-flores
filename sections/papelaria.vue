b49f9292c6b3895e5634f72b3c83e25b648a5849
Henrique Melo <hacmelo@gmail.com>
2022-03-29 14:20:30 -0300

    Refactor: papelaria

--- sections/papelaria.vue
+++ sections/papelaria.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="papelaria" id="papelaria">
-    <h1 class="papelaria-title">nossa papelaria</h1>
+    <!-- Galeria de fotos da papelaria -->
+    <span class="papelaria-title">nossa papelaria</span>
     <img
       class="papelaria-img"
       v-for="index in 7"
@@ -15,11 +16,12 @@ export default {
 };
 </script>
 <style scoped>
+/* Inicio papelaria */
 .papelaria {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 0.75rem;
-  padding: 0.75rem !important;
+  padding: 0.75rem;
 }
 
 .papelaria-title {
@@ -29,20 +31,20 @@ export default {
   background-color: #b69ca1;
   color: #fff;
   text-transform: uppercase;
-  font-weight: normal;
-  font-size: clamp(1.5rem, 3.5vw, 2rem);
+  font-size: clamp(1.5rem, 2vw, 2rem);
   text-align: center;
-  margin: 0 auto !important;
-  width: 100%;
-  padding: 1rem !important;
+  padding: 1rem;
 }
+
 .papelaria-img {
   aspect-ratio: 1;
   width: 100%;
   object-position: center;
   object-fit: cover;
 }
+/* Fim papelaria */
 
+/* Inicio media queries */
 @media (max-width: 900px) {
   .papelaria {
     grid-template-columns: repeat(2, 1fr);
@@ -55,4 +57,5 @@ export default {
     grid-auto-rows: 1fr;
   }
 }
+/* Fim media queries */
 </style>
